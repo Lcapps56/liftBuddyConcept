96eb8f25f4a36539362bab43135a4615c4b8da34
lcapps <lcapps5600@gmail.com>
2019-09-22 13:22:35 -0400

    outlined next issues

--- public/js/myworkouts.js
+++ public/js/myworkouts.js
@@ -1,33 +1,46 @@
-$.ajax({
-    type: "GET",
-    url: "/api/retreiveR"
-}).then(function(response){
-    for(let i=0; i<response.length; i++){        
-        var newDiv = $("<div id ='workoutBox'></div>")
-        var newTitle = $("<h1 class='Rtitle'>" + response[i].Title + "</h1>")
-        var newMuscles = $("<p class='Rmuscles'> <b>Muscle group:</b> " + response[i].Muscles + "</p>")
-        var newDifficulty = $("<p class='Rdifficulty'> <b>Difficulty:</b> " +  response[i].Difficulty + "/10</p>")
-        var newButton = $("<a data-title='"+ response[i].Title +"'id='viewW'><button class='view'>View workout</button></a>")
-        $(newDiv).append(newTitle, newMuscles, newDifficulty, newButton)
-        $("#myworkouts").append(newDiv)
-    }
-    console.log(response)
-    $("#viewW").on("click", function(){
+onLoad()
+
+
+function onLoad() {
+    $.ajax({
+        type: "GET",
+        url: "/api/retreiveR"
+    }).then(function(response){
+        for(let i=0; i<response.length; i++){        
+            var newDiv = $("<div id ='workoutBox'></div>")
+            var newTitle = $("<h1 class='Rtitle'>" + response[i].Title + "</h1>")
+            var newMuscles = $("<p class='Rmuscles'> <b>Muscle group:</b> " + response[i].Muscles + "</p>")
+            var newDifficulty = $("<p class='Rdifficulty'> <b>Difficulty:</b> " +  response[i].Difficulty + "/10</p>")
+            var newButton = $("<a data-title='"+ response[i].Title +" 'id='viewW' class='view'>View workout</a>")
+            $(newDiv).append(newTitle, newMuscles, newDifficulty, newButton)
+            $("#myworkouts").append(newDiv)
+        }
+        console.log(response)
+    })
+}
+
+$(document).ready(function(){
+    $("a #viewW").on("click", function(){
+        alert("clicked")
         $(this).attr({
             href: "#ex1", 
             rel: "modal:open"
         })
-        // modal text
-        $.ajax({
-            type: "GET", 
-            url: "/api/Rmodal/" + $(this).data("title")
-        }).then(function(response){
-            console.log("========")
-            console.log(response)
-            console.log("========")
-        })
     })
 })
+
+    // modal text
+    // $.ajax({
+    //     type: "GET", 
+    //     url: "/api/Rmodal/" + $(this).data("title")
+    // }).then(function(response){
+    //     console.log("========")
+    //     console.log(response)
+    //     console.log("========"
+    // })
+
       
+
+
 
 
