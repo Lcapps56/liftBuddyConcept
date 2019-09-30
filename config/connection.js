5e5515a537fd96a36e967fea5c46b116d4469722
lcapps <lcapps5600@gmail.com>
2019-09-30 12:43:51 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("liftBuddy", "root", "root", {
-    host: "localhost",
+
+var sequelize = new Sequelize("b1tncoawj1gg9mum", "h53esj90mvatp4oq", "k61qzo0r2vfhze3z", {
+    host: "g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     dialect: "mysql",
     pool: {
